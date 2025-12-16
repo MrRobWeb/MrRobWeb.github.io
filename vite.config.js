d9f1308ddb4b9f63b781f2c685e87258cccd8ea2
robert.weber <robert.weber@accenture.com>
2025-12-16 10:33:42 +0100

    Add jpg/jpeg to PWA cache patterns

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,jpg,jpeg,svg,woff2}'],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
